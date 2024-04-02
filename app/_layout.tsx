bb185fff84c439f4bea9ea07f6ff03d2e0d7ca72
mohammadNoruzi <mohammadnorozy77@gmail.com>
2024-04-03 01:05:36 +0330

    add Dish component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -69,6 +69,35 @@ export default function RootLayoutNav() {
             ),
           }}
         />
+        <Stack.Screen
+          name="(modal)/dish"
+          options={{
+            presentation: "modal",
+            headerTitle: "",
+            gestureEnabled: true,
+
+            headerTransparent: true,
+
+            headerLeft: () => (
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "#fff",
+                  borderRadius: 20,
+                  padding: 6,
+                }}
+                onPress={() => {
+                  navigation.goBack();
+                }}
+              >
+                <Ionicons
+                  name="close-outline"
+                  size={28}
+                  color={Colors.primary}
+                />
+              </TouchableOpacity>
+            ),
+          }}
+        />
       </Stack>
     </BottomSheetModalProvider>
   );
