0d8b62678377a7514f08de0ab913fc9f2c3a1ead
mohammadNoruzi <mohammadnorozy77@gmail.com>
2024-05-02 20:25:08 +0330

    profile

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,14 +1,34 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
+    <SafeAreaView edges={["bottom"]} style={styles.container}>
+      <View style={styles.profilePhoto}></View>
+      <View style={styles.profileInformation}></View>
+      <View style={styles.validate}></View>
+    </SafeAreaView>
   );
 };
 
 export default profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    // backgroundColor: "red",
+    flex: 1,
+  },
+  profilePhoto: {
+    // backgroundColor: "red",
+    flex: 3,
+  },
+  profileInformation: {
+    // backgroundColor: "green",
+    flex: 3,
+  },
+  validate: {
+    // backgroundColor: "yellow",
+    flex: 1,
+  },
+});
